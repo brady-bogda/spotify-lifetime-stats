de054c8797c5832b53a5e07f82fdd51339844911
bbogda <bbogda@umich.edu>
2023-08-11 20:27:02 -0400

    moved styled components

---
+++ spot-stats/src/App.style.tsx
@@ -0,0 +1,28 @@
+import { styled } from '@mui/system';
+import { Paper, TextField, Button } from '@mui/material';
+
+export const StyledContainer = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    background: 'linear-gradient(to right bottom, #93E9BE, #EEFFFF)',
+  });
+  
+export const StyledPaper = styled(Paper)({
+    padding: '16px',
+    width: '300px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  });
+  
+export const StyledTextField = styled(TextField)({
+    margin: '8px',
+    width: '100%',
+  });
+  
+export const StyledButton = styled(Button)({
+    margin: '16px 0',
+  });
--- spot-stats/src/App.tsx
+++ spot-stats/src/App.tsx
@@ -1,35 +1,9 @@
 import React, { useState } from 'react';
-import { styled } from '@mui/system';
-import { Container, Paper, TextField, Button, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { StyledContainer, StyledPaper, StyledTextField, StyledButton } from './App.style';
 
 import {Dashboard} from './Dashboard';
-
-const StyledContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100vh',
-  background: 'linear-gradient(to right bottom, #93E9BE, #3949ab)',
-});
-
-const StyledPaper = styled(Paper)({
-  padding: '16px',
-  width: '300px',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  backgroundColor: 'rgba(255, 255, 255, 0.8)',
-});
-
-const StyledTextField = styled(TextField)({
-  margin: '8px',
-  width: '100%',
-});
-
-const StyledButton = styled(Button)({
-  margin: '16px 0',
-});
 
 export const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
